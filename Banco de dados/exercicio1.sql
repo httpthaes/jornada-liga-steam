2f5342cc3ca3d60498a00d181f554fd18b62b128
Thais <thaiserica1304@gmail.com>
2026-06-30 15:10:44 -0300

    inserindo dados na tabela aluno

--- Banco de dados/exercicio1.sql
+++ Banco de dados/exercicio1.sql
@@ -2,9 +2,16 @@ CREATE DATABASE ESCOLA;
 
 USE ESCOLA;
 
-CREATE TABLE aluno (
+CREATE TABLE ALUNO (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE,
     endereco VARCHAR(255)
-)
+);
+
+INSERT INTO ALUNO (nome, email, endereco) VALUES 
+    ('Luciana', 'luciana@email.com', 'Rua São José, 123'),
+    ('José Maria', 'josemaria@email.com', 'Rua Domingos Vaz, 856'),
+    ('Suzana', 'suzana@email.com', 'Rua Santa Luzia, 756');
+
+SELECT * FROM ALUNO;
